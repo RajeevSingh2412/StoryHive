0b05f338c4610e133a902458a8007741d64910de
RajeevSingh2412 <118631381+RajeevSingh2412@users.noreply.github.com>
2023-07-17 16:12:48 +0530

    initial commit

---
+++ Home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Story Writter</title>
+  </head>
+  <body>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <i class="fa-thin fa-pen fa-bounce"></i>
+
+            </div>
+        </div>
+    </header>
+
+
+
+
+
+
+
+	<script src="index.js"></script>
+  </body>
+</html>
+
+
+<style>
+    body{
+        background-color:#0F1111;
+        margin:0px;
+    }
+    .navbar{
+        width:100%;
+        height:70px;
+        background-color:#1b1f1f;
+        display:flex;
+
+    }
+    .logo{
+        display:flex;
+        align-items:center;
+        width:15%;
+        align-items:center;
+
+    }
+
+</style>
